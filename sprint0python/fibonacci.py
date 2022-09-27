cd6bc07a6e06549b222bda17c06eb3dbbe12b66f
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-09-27 09:20:13 +0200

    Corregido error ej3

--- sprint0python/fibonacci.py
+++ sprint0python/fibonacci.py
@@ -8,5 +8,5 @@ numero = int(input("Introduce el enésimo término de la sucesión de fibonacci 
 while(numero<0):
 	if(numero<0):
 		print("El número introducido no es positivo")
-	opcion = input("Introduce el enésimo término de la sucesión de fibonacci que quieres calcular: ")
+	numero = int(input("Introduce el enésimo término de la sucesión de fibonacci que quieres calcular: "))
 print(str(funcion_fibonacci(numero)))
