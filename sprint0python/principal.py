6411bdf80a478091bccd17f9b2777aa34d4443ad
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-09-27 10:02:17 +0200

    Modificado principal.py (Ejercicio 6)

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,5 +1,6 @@
 from fibonacci import funcion_fibonacci
 from fibonacci2 import funcion_fibonacci2
+import time
 
 opcion=input("""Introduce una opcion
 a) Función fibonacci 1 (Recursiva)
@@ -15,11 +16,20 @@ if(opcion=="a"):
 		if(numero<0):
 			print("El número introducido no es positivo")
 		numero = int(input("Introduce el enésimo término de la sucesión de fibonacci que quieres calcular: "))
-	print(str(funcion_fibonacci(numero)))
+	start_time = time.time()
+	print(funcion_fibonacci(numero))
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 else:
 	numero = int(input("Introduce el enésimo término de la sucesión de fibonacci que quieres calcular: "))
+	
 	while(numero<0):
 		if(numero<0):
 			print("El número introducido no es positivo")
 		numero = int(input("Introduce el enésimo término de la sucesión de fibonacci que quieres calcular: "))
-	print(str(funcion_fibonacci2(numero)))
+	start_time = time.time()
+	print(funcion_fibonacci2(numero))
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
