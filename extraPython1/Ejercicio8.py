b3f5a22a5e80402789bbdbdbcba7eb53cf32e8aa
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-10-11 08:39:29 +0200

    Ejercicio 8

---
+++ extraPython1/Ejercicio8.py
@@ -0,0 +1,7 @@
+def sPos(a, b):
+    for i in range(len(a)):
+        for j in range(len(b)):
+            if (a[i] == b[j]):
+                return True
+            else:
+                return False
