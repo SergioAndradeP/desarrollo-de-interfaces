ec3d541db645c1f4947479b75049a8e7811a0aa6
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-10-11 08:34:44 +0200

    Ejercicio 5

---
+++ extraPython1/Ejercicio5.py
@@ -0,0 +1,14 @@
+
+
+def suma(a):
+    total = 0
+    for i in range(len(a)):
+        total += a[i]
+    return total
+
+
+def mult(a):
+    total = 1
+    for i in range(len(a)):
+        total *= a[i]
+    return total
