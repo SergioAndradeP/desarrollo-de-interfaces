9242fd66d60dfcd3a7e20b1dda344abdccdbbb70
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-10-18 09:09:41 +0200

    Ejercicio 2

--- sprint2catalog/main.py
+++ sprint2catalog/main.py
@@ -2,8 +2,10 @@ import gi # Imports necesarios
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from window import MainWindow
+from load_window import LoadWindow
 
-win = MainWindow()  # Generamos una instancia de MainWindow
+#win = MainWindow()  # Generamos una instancia de MainWindow
+win = LoadWindow()
 win.show_all() # Mostramos la ventana y todos sus componentes
 
 Gtk.main() # Hacemos que no se cierre mediante esta función
