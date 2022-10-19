d5f3ca97aeac4470260814cc107f6a927404bccc
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-10-19 10:53:02 +0200

    Ejercicio 6 'El misterio de las cajas'

--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -26,8 +26,35 @@ class MainWindow(Gtk.Window): #Herencia de Window
         scrolled = Gtk.ScrolledWindow() # Generamos una instancia de ScrolledWindow
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         scrolled.add(self.flowbox) # Añadimos a la ScrolledWindow nuestra FlowBox
-        self.add(scrolled) # Añadimos a la ventana la ScrolledWindow que ya contiene la FlowBox dentro
+        #self.add(scrolled) # Añadimos a la ventana la ScrolledWindow que ya contiene la FlowBox dentro
+
+        mb = Gtk.MenuBar()
+
+        filemenu = Gtk.Menu()
+        filem = Gtk.MenuItem("Ayuda")
+        filem.set_submenu(filemenu)
+
+        acercaDe = Gtk.MenuItem("Acerca De")
+        acercaDe.connect("activate", self.click)
+        filemenu.append(acercaDe)
+        mb.append(filem)
+
+        vbox = Gtk.VBox(False, 2)
+        vbox.pack_start(mb, False, False, 0)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.pack_start(vbox, False, False, 0)
+        box.pack_start(scrolled, False, False, 0)
+        #vbox.pack_start(scrolled, False, False, 0)
+        self.add(box)
 
         for item in data_source:
             cell = Cell(item.get("name"), item.get("gtk_image"), item.get("description"))
             self.flowbox.add(cell)
+
+    def click(self):
+        win = Gtk.Window(title = "Sobre el desarrollador")
+        win.show_all()
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        label = Gtk.Label("SAMPLE TEXT")
+        box.pack_start(label, True, True, 0)
+        Gtk.main()
