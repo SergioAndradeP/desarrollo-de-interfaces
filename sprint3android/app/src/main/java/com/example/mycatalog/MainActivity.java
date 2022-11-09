ab3bf87f5027a1f156324b862239216fd310e9c7
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-11-09 10:38:08 +0100

    Ejercicio 5 casi terminado

---
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mycatalog;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private Menu menu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigator_layout);
+        toolbar = findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+
+        menu = navigationView.getMenu();
+        navigationView.getHeaderView(0);
+        navigationView.bringToFront();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigator_open, R.string.navigator_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                break;
+            case R.id.catalog:
+                break;
+            case R.id.about:
+                break;
+        }
+        return false;
+    }
+}
