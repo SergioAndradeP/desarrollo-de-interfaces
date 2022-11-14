6026ef35151d02332a52529f5320c694549c659b
Sergio Andrade <sandradep@fpcoruna.afundacion.org>
2022-11-14 17:03:34 +0100

    Last push

---
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogFragment.java
@@ -0,0 +1,31 @@
+package com.example.mycatalog;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CatalogFragment extends Fragment {
+    private Button button;
+    private Context context = getContext();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_catalog, container, false);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        return view;
+    }
+}
--- sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -1,52 +1,65 @@
 package com.example.mycatalog;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Context context=this;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
     private Menu menu;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigator_layout);
-        toolbar = findViewById(R.id.toolbar_id);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.navigator_layout);
+        toolbar=findViewById(R.id.toolbar_id);
         setSupportActionBar(toolbar);
 
-        menu = navigationView.getMenu();
+        menu=navigationView.getMenu();
         navigationView.getHeaderView(0);
         navigationView.bringToFront();
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigator_open, R.string.navigator_close);
+
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.navigator_open, R.string.navigator_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setCheckedItem(R.id.home);
+        navigationView.setCheckedItem(R.id.catalog);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.home:
-                break;
+        switch(item.getItemId()){
             case R.id.catalog:
+                Fragment fragmentCatalog = new CatalogFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragmentCatalog).commit();
                 break;
             case R.id.about:
+                Fragment fragmentAbout = new AboutFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragmentAbout).commit();
                 break;
         }
         return false;
